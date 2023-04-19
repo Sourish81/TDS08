f7fa0b0c374222f4c50cbeac90b3c3fc78f306e2
Sourish81 <130020492+Sourish81@users.noreply.github.com>
2023-04-19 16:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
-
-st.write(""Find the Largest Number
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+Print(""Find the Largest Number
 "")
 
 
