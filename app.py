3ae9671a506412096d695b4c4f1d02bdae1a2e62
Sourish81 <130020492+Sourish81@users.noreply.github.com>
2023-04-19 16:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+st.header('Find the Largest Number.')
 
 st.write("""Enter three numbers and find the largest among them
 
 """)
 
-st.header('Find the Largest Number.')
+
 
 def compute_max(a,b,c):
     l=list([a,b,c])
