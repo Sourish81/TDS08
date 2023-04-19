86c03228b13bff264bf831e992e0d3cc540f4ee8
Sourish81 <130020492+Sourish81@users.noreply.github.com>
2023-04-19 16:30:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ st.header('Find the Largest Number.')
 st.write("""Enter three numbers and find the largest among them
 
 """)
-
-
-
+def user_input_features():
+     data={'Enter Number 1': a,
+           'Enter Number 2': b,
+           'Enter Number 3': c,
+          }
+     features = pd.DataFrame(data, index=[0])
+     return features
+    
 def compute_max(a,b,c):
     l=list([a,b,c])
     return max(l)
