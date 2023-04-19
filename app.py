3c3d430d700f0f0c5fc66596d6fbaca38d5cf0cc
Sourish81 <130020492+Sourish81@users.noreply.github.com>
2023-04-19 16:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
+
 
 
 st.header('Enter three numbers and find the largest among them.')
